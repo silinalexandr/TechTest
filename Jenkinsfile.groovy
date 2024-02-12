ed66255d82bc6a094a3e935dc534ef3e975525a9
Alexander <silinalexandr@gmail.com>
2024-02-12 17:27:39 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,10 +2,41 @@
 
 @Library('shared_library') _
 
+import main.groovy.ru.sbrf.example.shared_library.Pipeline
+import main.groovy.ru.sbrf.example.shared_library.Stage
+
+class HelloWorldStage extends Stage {
+
+    HelloWorldStage(String name) { super(name) }
+
+    @Override
+    protected void execute(Script script) {
+        logging(script, 'Hello world')
+    }
+}
+
+Pipeline pipeline = new Pipeline(
+    this,
+    [
+        new HelloWorldStage('Hello world')
+    ]
+)
+pipeline.execute()
+
 pipeline {
     agent any
 
     stages {
+        stage('Execute Shared Pipeline') {
+            steps {
+                script {
+                    executePipeline()
+                }
+            }
+        }
+
+    
+ stages {
         stage('Clone Repository') {
             steps {
                 script {
@@ -29,6 +60,9 @@ pipeline {
                 }
             }
         }
+    }
+   
+
     }
 
     post {
